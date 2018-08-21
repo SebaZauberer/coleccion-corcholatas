2a2ff53e58572fdb11cf5f93ffab9f55a2fcf81e
Sebastian Olate H <seba.olate.h@gmail.com>
2018-08-21 01:21:56 -0300

    cambios primer intendo de conneccion de base de datos

--- js/add.js
+++ js/add.js
@@ -3,7 +3,7 @@ var form = document.getElementById('add-form');
 document.addEventListener('DOMContentLoaded', function () {
     var anio = form.anio;
 
-    for (var i = 2018; i >= 2016; i--) {
+    for (var i = 2018; i >= 2015; i--) {
         var optionAnio = document.createElement('option');
         optionAnio.value = i;
         optionAnio.innerText = i;
@@ -13,11 +13,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function validation() {
     if (form.marca == null || form.marca.value.length == 0 || /^\s+$/.test(form.marca.value)) {
-        alert("Ingresa un texto valido");
+        alert("Ingresa una texto valido en el campo marca");
         return false;
     }
     if (/^\s+$/.test(form.variedad.value)) {
-        alert("Ingresa un texto valido");
+        alert("Ingresa un texto valido en el campo variedad");
         return false;
     } else {
         if (form.variedad == null || form.variedad.value.length == 0) {
@@ -25,7 +25,7 @@ function validation() {
         }
     }
     if (form.pais == null || form.pais.value.length == 0 || /^\s+$/.test(form.pais.value)) {
-        alert("Ingresa un texto valido");
+        alert("Ingresa un texto valido en el campo país");
         return false;
     }
     if (form.anio.selectedIndex == null) {
@@ -33,10 +33,10 @@ function validation() {
         return false;
     }
     if (form.ruta == null || form.ruta.value.length == 0 || /^\s+$/.test(form.ruta.value)) {
-        alert("Ingresa un ruta valida");
+        alert("Ingresa un ruta valida de imagen");
         return false;
     }
-    if (/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(form.sitioWeb.value)) {
+    if (!/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(form.sitioWeb.value)) {
         alert("Ingresa una URL valida");
         return false;
     }
@@ -47,27 +47,29 @@ function validation() {
 
 function addChapa() {
 
-    if (validation()) {
-        var urlData = 'http://localhost:5000/make';
-        var data = {
-            brand: form.marca.value,
-            country: form.pais.value,
-            type: form.variedad.value,
-            year: form.anio.selectedIndex,
-            image: form.ruta.value,
-            website: form.sitioWeb.value
-        }
+    var urlData = 'http://localhost:5000/make';
+    var data = {
+        brand: form.marca.value,
+        country: form.pais.value,
+        type: form.variedad.value,
+        year: form.anio.value,
+        image: form.ruta.value,
+        website: form.sitioWeb.value
+    }
 
+    if(validation()){
         fetch(urlData, {
             method: 'POST',
             body: JSON.stringify(data),
             headers:{
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             }
-          }).then(res => res.json())
-          .catch(error => console.error('Error:', error))
-          .then(response => console.log('Success:', response));
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+
+        return true;
     }
+    
 }
 
-form.agregar.addEventListener('submit', addChapa);
---
+++ js/main.js
@@ -0,0 +1,37 @@
+function load() {
+    fetch("http://localhost:5000/find")
+        .then(res => res.json())
+        .then(response => {
+            console.log(response);
+
+            var record = getElementById('record');
+            record.innerText = response.response.length;
+
+            var chapitas = document.getElementById('chapitas-list');
+            var list = document.createElement('ul');
+
+            chapitas.appendChild(list);
+
+            response.response.forEach(element => {    
+                var content = document.createElement('li');
+                var link = document.createElement("a");
+                var img = document.createElement('img');
+                var imgContent = `
+                <div class="look">
+                    <img src="img/lupa.png" alt="ver chapita">
+                </div>`;
+                
+                img.alt = element.brand;
+                img.src = element.img;
+                link.href = "detail.html?id=" + element.id;
+
+                content.appendChild(link);
+                link.appendChild(img);
+                link.appendChild(imgContent);
+
+                list.appendChild(content);
+            });
+        })
+}
+
+document.addEventListener("DOMContentLoaded", load);
